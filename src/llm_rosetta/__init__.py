5282bdb064bea34564c71b6d046a8552494d3f44
Peng Ding <pding.dp@foxmail.com>
2026-04-10 23:30:23 +0800

    release: bump version to 0.4.1

--- src/llm_rosetta/__init__.py
+++ src/llm_rosetta/__init__.py
@@ -21,7 +21,7 @@ from .converters import (
 )
 from .converters.base.context import ConversionContext, StreamContext
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 __all__ = [
     # 转换器 Converters
